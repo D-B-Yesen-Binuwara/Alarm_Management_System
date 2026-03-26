aa527dc526fe31c7b03bfb6eac5bec10b83754a8
S Thulasi <sa24610225@my.sliit.lk>
2026-03-26 18:54:51 +0530

    fixed src structure properly

--- src/service/index.js
+++
@@ -1,5 +0,0 @@
-// API service functions exports
-// Add your service exports here as you create them
-
-// export { default as apiClient } from './apiClient';
-// export { default as authService } from './authService';
