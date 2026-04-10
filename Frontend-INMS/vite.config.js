affc8622d8c2de9c0d7d2eb940380eb02995c34d
S Thulasi <sa24610225@my.sliit.lk>
2026-04-10 11:26:57 +0530

    Clean branch with correct backend from main

--- Frontend-INMS/vite.config.js
+++ Frontend-INMS/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         strictPort: true,
         proxy: {
             '/api': {
-                target: 'https://localhost:7257',
+                target: 'http://localhost:5289',
                 changeOrigin: true,
                 secure: false
             }
